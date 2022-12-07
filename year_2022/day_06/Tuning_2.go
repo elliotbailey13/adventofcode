c0cd2e5fd39c53c40cec55bbe3b68a9b193babe1
Elliot Bailey <elliot.bailey13@gmail.com>
2022-12-06 22:35:23 -0500

    2022 Day 6 **

---
+++ year_2022/day_06/Tuning_2.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+  "os"
+  "log"
+  "bufio"
+  "strings"
+)
+
+func main() {
+  Raw_Input, err := os.Open("./input.txt")
+  if err != nil { log.Fatal(err) }
+
+  var Full_Input []string
+  scanner := bufio.NewScanner(Raw_Input)
+  for scanner.Scan() {
+    Full_Input = append(Full_Input, scanner.Text())
+  }
+
+  if err := scanner.Err(); err != nil { log.Fatal(err) }
+
+  Raw_Input.Close()
+
+  Full_String_Array := []string{}
+  for _, Full_Input_Line := range Full_Input {
+    Full_String_Array = strings.Split(Full_Input_Line, "")
+  }
+
+  var Rolling_Array []string
+  for Index, Data := range Full_String_Array {
+    Rolling_Array = append(Rolling_Array, Data)
+
+    log.Printf("Index: %v - Data: %v - Rolling: %v", Index, Data, Rolling_Array)
+    if len(Rolling_Array) < 14 { continue }
+
+    Match := false
+    for Index_Data, Rolling_Data := range Rolling_Array {
+      if Match == true { continue }
+      for Index_Data_Search, Rolling_Data_Search := range Rolling_Array {
+        if Index_Data == Index_Data_Search { continue }
+        if Rolling_Data == Rolling_Data_Search {
+          Match = true
+          Rolling_Array = Rolling_Array[1:]
+          continue
+        }
+      }
+    }
+
+    if Match == false {
+      log.Printf("Marker found at %v", Index + 1)
+      log.Printf("Full Length: %v", len(Full_String_Array))
+      break
+    }
+  }
+}
